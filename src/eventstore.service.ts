df17f0a4590916e79009d4fee2740d65785fce53
Roman <s91630277@gmail.com>
2024-03-24 19:59:08 +0400

    fix: Dispose eventstore client on module destroy

--- src/eventstore.service.ts
+++ src/eventstore.service.ts
@@ -1,10 +1,10 @@
 import {
-    END,
-    EventStoreDBClient,
-    FORWARDS,
-    jsonEvent,
-    ResolvedEvent,
-    START,
+  END,
+  EventStoreDBClient,
+  FORWARDS,
+  jsonEvent,
+  ResolvedEvent,
+  START,
 } from '@eventstore/db-client';
 import { Injectable } from '@nestjs/common';
 import { TransformService } from './transform.service';
@@ -12,86 +12,90 @@ import { Event } from './event';
 import { AppendToStreamOptions } from './interfaces';
 
 export type AppendResult = {
-    /**
-     * The current revision of the stream, to be passed as the `expectedRevision` in the next call.
-     */
-    expectedRevision: bigint;
+  /**
+   * The current revision of the stream, to be passed as the `expectedRevision` in the next call.
+   */
+  expectedRevision: bigint;
 };
 
 @Injectable()
 export class EventStoreService {
-    constructor(
-        private readonly client: EventStoreDBClient,
-        private readonly transformService: TransformService,
-    ) {}
+  constructor(
+    private readonly client: EventStoreDBClient,
+    private readonly transformService: TransformService,
+  ) {}
 
-    async *readFromStart(stream: string): AsyncGenerator<Event, void> {
-        const streamingRead = this.client.readStream(stream, {
-            direction: FORWARDS,
-            fromRevision: START,
-        });
+  async *readFromStart(stream: string): AsyncGenerator<Event, void> {
+    const streamingRead = this.client.readStream(stream, {
+      direction: FORWARDS,
+      fromRevision: START,
+    });
 
-        for await (const { event } of streamingRead) {
-            if (!event) {
-                continue;
-            }
-            const transform = this.transformService.get(event.type);
-            yield transform?.(event) ?? event;
-        }
+    for await (const { event } of streamingRead) {
+      if (!event) {
+        continue;
+      }
+      const transform = this.transformService.get(event.type);
+      yield transform?.(event) ?? event;
     }
+  }
 
-    async appendToStream(
-        streamId: string,
-        events: Event | Event[],
-        options?: AppendToStreamOptions,
-    ): Promise<AppendResult> {
-        {
-            const expectedRevision = options?.expectedRevision;
-            const databaseEvents = (Array.isArray(events) ? events : [events]).map(
-                event => jsonEvent<any>(event),
-            );
+  async appendToStream(
+    streamId: string,
+    events: Event | Event[],
+    options?: AppendToStreamOptions,
+  ): Promise<AppendResult> {
+    {
+      const expectedRevision = options?.expectedRevision;
+      const databaseEvents = (Array.isArray(events) ? events : [events]).map(event =>
+        jsonEvent<any>(event),
+      );
 
-            const result = await this.client.appendToStream(streamId, databaseEvents, {
-                expectedRevision,
-            });
-            return {
-                expectedRevision: result.nextExpectedRevision,
-            };
-        }
+      const result = await this.client.appendToStream(streamId, databaseEvents, {
+        expectedRevision,
+      });
+      return {
+        expectedRevision: result.nextExpectedRevision,
+      };
     }
+  }
 
-    subscribeToStream(
-        streamId: string,
-        listener: (event: Event) => void,
-    ): () => Promise<void> {
-        const subscription = this.client.subscribeToStream(streamId);
+  subscribeToStream(
+    streamId: string,
+    listener: (event: Event) => void,
+  ): () => Promise<void> {
+    const subscription = this.client.subscribeToStream(streamId);
 
-        subscription.on('data', this.createEventstoreEventHandler(listener));
+    subscription.on('data', this.createEventstoreEventHandler(listener));
 
-        // eslint-disable-next-line @typescript-eslint/require-await
-        return async () => {
-            subscription.off('data', listener);
-        };
-    }
+    // eslint-disable-next-line @typescript-eslint/require-await
+    return async () => {
+      subscription.off('data', listener);
+    };
+  }
 
-    subscribeToAll(listener: (event: Event) => void): () => Promise<void> {
-        const subscription = this.client.subscribeToAll({ fromPosition: END });
-        subscription.on('data', this.createEventstoreEventHandler(listener));
+  subscribeToAll(listener: (event: Event) => void): () => Promise<void> {
+    const subscription = this.client.subscribeToAll({ fromPosition: END });
+    subscription.on('data', this.createEventstoreEventHandler(listener));
 
-        return () => subscription.unsubscribe();
-    }
+    return () => subscription.unsubscribe();
+  }
 
-    private createEventstoreEventHandler(listener: (event: Event) => void) {
-        return ({ event }: ResolvedEvent) => {
-            if (!event) {
-                return;
-            }
+  async onModuleDestroy() {
+    await this.client.dispose();
+  }
 
-            const transform = this.transformService.get(event.type);
-            // listener(transform?.(event) ?? event);
-            if (transform) {
-                listener(transform(event));
-            }
-        };
-    }
+  private createEventstoreEventHandler(listener: (event: Event) => void) {
+    return ({ event }: ResolvedEvent) => {
+      if (!event) {
+        return;
+      }
+
+      const transform = this.transformService.get(event.type);
+      // listener(transform?.(event) ?? event);
+      if (transform) {
+        listener(transform(event));
+      }
+    };
+  }
 }
